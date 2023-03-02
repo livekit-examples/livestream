f1e2e457510d8b8e0ba10a926e3cf60bd4560bcd
mattherzog <Herzog.Matt@gmail.com>
2023-03-02 11:44:42 -0800

    Updates watching bar blue to match other blues

--- src/components/channel/WatchingAsBar.tsx
+++ src/components/channel/WatchingAsBar.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export default function WatchingAsBar({ viewerName }: Props) {
   return (
-    <div className="border-b py-2 text-center text-xs text-blue-800 dark:border-b-0 dark:bg-blue-900 dark:text-blue-300">
+    <div className="border-b py-2 text-center text-xs text-blue-500 dark:border-b-0 dark:bg-blue-900 dark:text-blue-300">
       Watching as <span className="font-bold">{viewerName}</span>
     </div>
   );
