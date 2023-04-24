c55befa92bd9ff6b3be99bd2a8c9ee44167c43d5
Neil Dwyer <neildwyer1991@gmail.com>
2023-04-24 14:34:13 -0700

    Add deploy to Vercel button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,6 +34,18 @@ export default function IndexPage() {
             .
           </p>
           <p>
+            Want to deploy this sample app yourself? Our friends at Vercel make
+            it super easy:
+            <a href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Flivekit-examples%2Flivestream&env=LIVEKIT_API_KEY,LIVEKIT_API_SECRET,LIVEKIT_API_URL,NEXT_PUBLIC_LIVEKIT_WS_URL&envDescription=Sign%20up%20for%20an%20account%20at%20https%3A%2F%2Fcloud.livekit.io%20and%20create%20an%20API%20key%20in%20the%20Project%20Settings%20UI">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                className="mt-4"
+                alt="deploy with vercel"
+                src="https://vercel.com/button"
+              />
+            </a>
+          </p>
+          <p>
             To get started as a broadcaster, navigate to the{" "}
             <Link href="/setup" className="underline">
               setup page
