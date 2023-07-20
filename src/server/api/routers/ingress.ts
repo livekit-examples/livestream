6b94e4e9adb748187de95ff0ac37f90b1074f46b
Donald Huh <don@livekit.io>
2023-07-20 14:55:55 -0700

    fix: viewer cannot see or hear livestream again (#8)

--- src/server/api/routers/ingress.ts
+++ src/server/api/routers/ingress.ts
@@ -29,11 +29,11 @@ export const ingressRouter = createTRPCRouter({
           participantName: input.streamerName,
           participantIdentity: input.roomSlug,
           video: {
-            source: TrackSource.SCREEN_SHARE,
+            source: TrackSource.CAMERA,
             preset: IngressVideoEncodingPreset.H264_1080P_30FPS_3_LAYERS,
           },
           audio: {
-            source: TrackSource.SCREEN_SHARE_AUDIO,
+            source: TrackSource.MICROPHONE,
             preset: IngressAudioEncodingPreset.OPUS_STEREO_96KBPS,
           },
         }
